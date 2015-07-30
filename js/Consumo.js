cb24482f4e1da35f4d6195b762f638145866d85b
Luan Castro <lu4n.k4str0@gmail.com>
2015-07-30 13:02:57 -0400

    Atualização de erro com keyword 'var'

--- js/Consumo.js
+++ js/Consumo.js
@@ -1,9 +1,10 @@
-
-   var y = 0;
+(function(w){
+	
+   var y = 0; 
 	
    function ligar_L_C() {
  	  var x = document.getElementById("CM").value;
- 	  var y += x;
+ 	  y += x; //removido o var daqui, para o acesso ser realizado do y da closure
 
 
 
@@ -12,3 +13,10 @@
     document.getElementById("CM").value = 0;
   
   }
+  
+  //aconselho criar um objeto para gerenciar isso, mas enquanto não tem, estou expondo as funcoes
+  
+  w.ligar_L_C = ligar_L_C;
+  w.desligar_L_C = desligar_L_C;
+  
+}(window));
